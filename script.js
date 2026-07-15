602bdb7a3ba72ddf09ce8274760ebacbe08ee470
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2026-07-15 11:40:01 +0000

    sssssssssssssssss

--- script.js
+++ script.js
@@ -31,9 +31,13 @@ const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQTSXYWZ';
 geraSenha();
 
 function geraSenha () {
-    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
-    numeroAleatorio = Math.floor(numeroAleatorio);
-    console.log(letrasMaiusculas[numeroAleatorio]);
+
+    for(let i = 0; i < tamanhaSenha; i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        console.log(letrasMaiusculas[numeroAleatorio]);
+    }
+    
 }
 
 campoSenha.value = letrasMaiusculas;
