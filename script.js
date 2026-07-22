935092d2b884be838f609677528ec5beb5525412
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2026-07-22 07:34:07 -0300

    sssssssssssssssss

--- script.js
+++ script.js
@@ -30,12 +30,23 @@ function aumentaTamanho(){
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
 
-console.log(checkbox[0].checked);
+for(i = 0; i < checkbox.length; i++
+    checkbox[i].onclick = geraSenha;
+)
 
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQTSXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%&?';
 geraSenha();
 
 function geraSenha () {
+    let alfabeto = '';
+    if(checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+
+    }
+
     let senha = '';
     for(let i = 0; i < tamanhaSenha; i++){
         let numeroAleatorio = Math.random()*letrasMaiusculas.length;
