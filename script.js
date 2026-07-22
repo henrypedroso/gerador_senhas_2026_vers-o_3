2dfcbfdb544106857fc91373db3a6e239154af3a
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2026-07-22 13:13:09 -0300

    ssssssssssssssssssssssss

--- script.js
+++ script.js
@@ -44,7 +44,18 @@ function geraSenha () {
     let alfabeto = '';
     if(checkbox[0].checked){
         alfabeto = alfabeto + letrasMaiusculas;
+    }
 
+    if(checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+
+    if(checkbox[2].checked){
+        alfabeto = alfabeto + numeros;
+    }
+
+    if(checkbox[3].checked){
+        alfabeto = alfabeto + simbolos;
     }
 
     let senha = '';
